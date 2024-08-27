7d2f88180709f4e344e1930593c9fa7c329d68f9
FabianQuirozH <fa.quirozh@duocuc.cl>
2024-08-27 12:56:05 -0400

    s agrega formControla registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  //podemos crear variables
+  //NOMBRE: TIPO = VALOR
+  //NOMBRE= CLASE(VALOR);
+  persona = new FormGroup({
+    rut:new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required, Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]), 
+    nombre:new FormControl('',[Validators.minLength(3),Validators.required,Validators.pattern("[a-z]{3,5}")]), 
+    fecha_nacimiento:new FormControl(), 
+    genero:new FormControl() 
 
-  constructor() { }
+  });
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
   }
 
+    //podemos crear metodos:
+    public registrar():void{
+      //validaciones? llamar un DAO? conversion?}
+      console.log(this.persona.value);
+      alert("registrado!");
+      this.router.navigate(['/login']);
+
+    }
 }
