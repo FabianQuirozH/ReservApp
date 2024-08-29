9026af9b45cc90d65201a87fab204d593caa83c0
fabianquirozh <fa.quirozh@duocuc.cl>
2024-08-29 12:03:25 -0400

    se agrgaron validaciones

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -13,12 +13,30 @@ export class RegistroPage implements OnInit {
   //NOMBRE= CLASE(VALOR);
   persona = new FormGroup({
     rut:new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required, Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]), 
-    nombre:new FormControl('',[Validators.minLength(3),Validators.required,Validators.pattern("[a-z]{3,5}")]), 
-    fecha_nacimiento:new FormControl(), 
-    genero:new FormControl() 
-
+    nombre:new FormControl('',[Validators.required,Validators.pattern("[a-z-A-Z]{3,5}")]), 
+    fecha_nacimiento:new FormControl('',[Validators.required,]), 
+    genero:new FormControl('',[Validators.required]) //el required es para que sea obligatorio el dato 
+    //validar la fecha de nacimiento creando un metodo,
   });
 
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
+
+    //variables arriba del constructor
   constructor(private router: Router) {}
 
   ngOnInit() {
@@ -32,4 +50,18 @@ export class RegistroPage implements OnInit {
       this.router.navigate(['/login']);
 
     }
+
+    setResult(ev:any) {
+      console.log(`Dismissed with role: ${ev.detail.role}`);
+    }
+
+    /* metodo para validar boton
+    public habilitar_boton():boolean{
+      if(this.persona.valid){
+        return false;
+      }
+      return true;
+    }*/
+   
+
 }
